742ac5064ef53c2064685c2f07fdd5096058f09f
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-08-20 21:31:28 +0200

    case uttn

--- src/pages/orders/new.jsx
+++ src/pages/orders/new.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/display-name */
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import getConfig from "next/config";
 import { useRouter } from "next/router";
 import { parseCookies } from "nookies";
+import Quagga from 'quagga';
 
 import SectionTitle from '../../components/elements/section-title/index';
 import Widget from '../../components/elements/widget/index';
@@ -48,6 +49,130 @@ export default function Documents({
   const [name, setName] = useState("")
   const [status, setStatus] = useState("open")
 
+  const firstUpdate = useRef(true);
+  const [isStart, setIsStart] = useState(false);
+  const [barcode, setBarcode] = useState('');
+
+  useEffect(() => {
+    return () => {
+      if (isStart) stopScanner();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+
+    if (isStart) startScanner();
+    else stopScanner();
+  }, [isStart]);
+
+  const _onDetected = res => {
+    // stopScanner();
+    setBarcode(res.codeResult.code);
+  };
+
+  const startScanner = () => {
+    Quagga.init(
+      {
+        inputStream: {
+          type: 'LiveStream',
+          target: document.querySelector('#scanner-container'),
+          constraints: {
+            facingMode: 'environment' // or user
+          }
+        },
+        numOfWorkers: navigator.hardwareConcurrency,
+        locate: true,
+        frequency: 1,
+        debug: {
+          drawBoundingBox: true,
+          showFrequency: true,
+          drawScanline: true,
+          showPattern: true
+        },
+        multiple: false,
+        locator: {
+          halfSample: false,
+          patchSize: 'large', // x-small, small, medium, large, x-large
+          debug: {
+            showCanvas: false,
+            showPatches: false,
+            showFoundPatches: false,
+            showSkeleton: false,
+            showLabels: false,
+            showPatchLabels: false,
+            showRemainingPatchLabels: false,
+            boxFromPatches: {
+              showTransformed: false,
+              showTransformedBox: false,
+              showBB: false
+            }
+          }
+        },
+        decoder: {
+          readers: [
+            'code_128_reader',
+            'ean_reader',
+            'ean_8_reader',
+            'code_39_reader',
+            'code_39_vin_reader',
+            'codabar_reader',
+            'upc_reader',
+            'upc_e_reader',
+            'i2of5_reader',
+            'i2of5_reader',
+            '2of5_reader',
+            'code_93_reader'
+          ]
+        }
+      },
+      err => {
+        if (err) {
+          return console.log(err);
+        }
+        Quagga.start();
+      }
+    );
+    Quagga.onDetected(_onDetected);
+    Quagga.onProcessed(result => {
+      let drawingCtx = Quagga.canvas.ctx.overlay,
+        drawingCanvas = Quagga.canvas.dom.overlay;
+
+      if (result) {
+        if (result.boxes) {
+          drawingCtx.clearRect(
+            0,
+            0,
+            parseInt(drawingCanvas.getAttribute('width')),
+            parseInt(drawingCanvas.getAttribute('height'))
+          );
+          result.boxes.filter(box => box !== result.box).forEach(box => {
+            Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, {
+              color: 'green',
+              lineWidth: 2
+            });
+          });
+        }
+
+        if (result.box) {
+          Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+          Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+      }
+    });
+  };
+
+  const stopScanner = () => {
+    Quagga.offProcessed();
+    Quagga.offDetected();
+    Quagga.stop();
+  };
 
   const itemsTotal = [
     { title: 'Total Vat', element: <text>{vatTotal}</text> },
@@ -497,6 +622,15 @@ export default function Documents({
         </fieldset>
 
         <UnderlinedTabs tabs={tabs} />
+
+        <div>
+          <h3>Barcode scanner in React - <a href="https://www.cluemediator.com/" target="_blank" rel="noopener noreferrer">Clue Mediator</a></h3>
+          <button onClick={() => setIsStart(prevStart => !prevStart)} style={{ marginBottom: 20 }}>{isStart ? 'Stop' : 'Start'}</button>
+          {isStart && <React.Fragment>
+            <div id="scanner-container" />
+            <span>Barcode: {barcode}</span>
+          </React.Fragment>}
+        </div>
       </Widget>
 
 
